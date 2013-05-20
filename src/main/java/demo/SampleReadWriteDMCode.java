ba5c20c4c08ad1935d55e107a121d81f3137517b
vpryst <vpryst@IF030.softservecom.com>
2013-05-20 16:50:32 +0300

    Add SimpleReadwriteCide. for execute two type of Barcodde

--- src/main/java/demo/SampleReadWriteDMCode.java
+++ src/main/java/demo/SampleReadWriteDMCode.java
@@ -17,25 +17,25 @@ import com.google.zxing.ChecksumException;
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.FormatException;
-import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.datamatrix.DataMatrixWriter;
 import com.google.zxing.datamatrix.DataMatrixReader;
+import com.google.zxing.datamatrix.DataMatrixWriter;
 import com.google.zxing.datamatrix.encoder.SymbolShapeHint;
 
 public class SampleReadWriteDMCode {
     public static void main(String[] args) throws IOException {
+        String encodeData = "sdfsgdafbsdgfjhsdgahsdlgkjahsd;flhka;sdgkjha;dsfklHS;GJHA;SGFLJDHSFLKGJHS;DKLGFJA'FGHDFK 50e8400-e29b-41d4-a716-446655440000|4";
         Map<EncodeHintType,Object> hints = new EnumMap<EncodeHintType,Object>(EncodeHintType.class);
         hints.put(EncodeHintType.DATA_MATRIX_SHAPE, SymbolShapeHint.FORCE_SQUARE);
 
-        int bigEnough = 1020;
+        int bigEnough = 1000;
         DataMatrixWriter writer = new DataMatrixWriter();
-        BitMatrix matrix = writer.encode("Orest Ivasiv 50e8400-e29b-41d4-a716-446655440000", BarcodeFormat.DATA_MATRIX, bigEnough, bigEnough, hints);
+        BitMatrix matrix = writer.encode(encodeData, BarcodeFormat.DATA_MATRIX, bigEnough, bigEnough, hints);
 
         
         String fileName = "code.png";
@@ -44,7 +44,7 @@ public class SampleReadWriteDMCode {
         System.out.println("printing to " + file.getAbsolutePath());
         Desktop.getDesktop().open(file);
         
-    
+    /////////////////////////////////////////////////////////////////////////////////////////////////
         Map<DecodeHintType,Object> hintsRead = new EnumMap<DecodeHintType,Object>(DecodeHintType.class);
         hintsRead.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.DATA_MATRIX));
           hintsRead.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
@@ -54,9 +54,6 @@ public class SampleReadWriteDMCode {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    
-
-    
     }
     public static String readDMCode(String filename,
              Map<DecodeHintType,?> hints)
@@ -72,7 +69,6 @@ public class SampleReadWriteDMCode {
         } catch (FormatException e) {
             e.printStackTrace();
         }
-
         return result.getText();
     }
     
---
+++ src/main/java/demo/SimpleReadWriteBarCode.java
@@ -0,0 +1,96 @@
+package demo;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.datamatrix.encoder.SymbolShapeHint;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class SimpleReadWriteBarCode {
+
+    private static final String CHARSET_NAME = "UTF-8";
+    private static int width = 100;
+    private static int height = 100;
+
+    /**
+     * @param args
+     * @throws IOException
+     * @throws NotFoundException
+     * @throws WriterException
+     */
+    public static void main(String[] args) throws IOException, NotFoundException, WriterException {
+        String encodeData =
+            "sfthdfghsrgfsdf  sftghsfhygaergfs  KSDHJFGKLJSDHFGJDNBFZKJDHFGASDFBVN,B;slgkdh;fgjsdhfgjsdb;fns.d,mfgnd.sgjhkjghsdkjg 50e8400-e29b-41d4-a716-446655440000|4";
+
+        Map<EncodeHintType, Object> hintsQR = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+        hintsQR.put(EncodeHintType.CHARACTER_SET, CHARSET_NAME);
+        hintsQR.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q);
+
+        File fileQR = createFile("QRCode");
+        writeCode(fileQR, encodeData, BarcodeFormat.QR_CODE, width, height, hintsQR);
+        showFile(fileQR);
+
+        Map<DecodeHintType, Object> decodeHints = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
+        decodeHints.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.QR_CODE));
+        String dataOut = readCode(fileQR, decodeHints);
+        System.out.println(dataOut);
+
+        // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Map<EncodeHintType, Object> hintsDM = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+        hintsDM.put(EncodeHintType.DATA_MATRIX_SHAPE, SymbolShapeHint.FORCE_SQUARE);
+ 
+        File fileDM = createFile("DataMatrix");
+        writeCode(fileDM, encodeData, BarcodeFormat.DATA_MATRIX, width, height, hintsDM);
+        showFile(fileDM);
+        
+        // ///////////////////////////////////////////////////////////////////////////////////////////////
+        Map<DecodeHintType, Object> hintsRead = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
+        hintsRead.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.DATA_MATRIX));
+        hintsRead.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+        
+        String data = readCode(fileDM, hintsRead);
+        System.out.println(data);
+    }
+
+    private static void writeCode(File file, String data, BarcodeFormat barcode, int width, int height, Map<EncodeHintType, Object> hints) throws IOException, WriterException {
+        BitMatrix matrix =
+            new MultiFormatWriter().encode(data, barcode, width, height,
+                hints);
+        MatrixToImageWriter.writeToFile(matrix, "PNG", file);
+    }
+
+    private static String readCode(File file, Map<DecodeHintType, ?> hints) throws IOException, NotFoundException {
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(file))));
+        Result result = new MultiFormatReader().decode(binaryBitmap, hints);
+        return result.getText();
+    }
+
+    private static File createFile(String fileName) throws IOException {
+        File file = File.createTempFile(fileName, ".png");
+        return file;
+    }
+    private static void showFile(File file) throws IOException {
+        System.out.println("printing to " + file.getAbsolutePath());
+        Desktop.getDesktop().open(file);
+    }
+}
