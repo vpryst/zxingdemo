13c445f105dc25e303625e2e92b5cb1d8e41ee12
vpryst <vpryst@IF030.softservecom.com>
2013-05-21 14:23:40 +0300

    Added comments and delete not used files

--- src/main/java/demo/SimpleReadWriteBarCode.java
+++ src/main/java/demo/SimpleReadWriteBarCode.java
@@ -27,16 +27,29 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class SimpleReadWriteBarCode {
 
+    /**
+     * important if specified char set
+     */
     private static final String CHARSET_NAME = "UTF-8";
+    /**
+     * important for image size;
+     */
     private static int width = 100;
     private static int height = 100;
-    
+
+    /**
+     * String to encode
+     */
     private static String encodeData = "sgjhkjghsdkjg 50e8400-e29b-41d4-a716-446655440000|4";
-    
+
+    /**
+     * constructor to change text encode.
+     * 
+     * @param text
+     */
     public SimpleReadWriteBarCode(String text) {
         encodeData = text;
     }
-
 
     /**
      * @param args
@@ -45,56 +58,108 @@ public class SimpleReadWriteBarCode {
      * @throws WriterException
      */
     public static void main(String[] args) throws IOException, NotFoundException, WriterException {
-        
 
+        /**
+         * Settings for QRCode. Write to image
+         */
         Map<EncodeHintType, Object> hintsQR = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
         hintsQR.put(EncodeHintType.CHARACTER_SET, CHARSET_NAME);
         hintsQR.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q);
 
+        /**
+         * write to image
+         */
         File fileQR = createFile("QRCode");
         writeCode(fileQR, encodeData, BarcodeFormat.QR_CODE, width, height, hintsQR);
         showFile(fileQR);
-
+        /**
+         * Settings QRCode to read from image.
+         */
         Map<DecodeHintType, Object> decodeHints = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
         decodeHints.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.QR_CODE));
+        /**
+         * read QRCode
+         */
         String dataOut = readCode(fileQR, decodeHints);
         System.out.println(dataOut);
 
         // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        /**
+         * Settings for DataMatrix
+         */
         Map<EncodeHintType, Object> hintsDM = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
         hintsDM.put(EncodeHintType.DATA_MATRIX_SHAPE, SymbolShapeHint.FORCE_SQUARE);
-
+        /**
+         * write dataMatrix to image
+         */
         File fileDM = createFile("DataMatrix");
         writeCode(fileDM, encodeData, BarcodeFormat.DATA_MATRIX, width, height, hintsDM);
         showFile(fileDM);
 
-        // ///////////////////////////////////////////////////////////////////////////////////////////////
+        /**
+         * Settings for read DataMatrix
+         */
         Map<DecodeHintType, Object> hintsRead = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
         hintsRead.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.DATA_MATRIX));
-        hintsRead.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
-
+        // hintsRead.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+        /**
+         * read DataMatrix
+         */
         String data = readCode(fileDM, hintsRead);
         System.out.println(data);
     }
 
+    /**
+     * Method to write BarCode to image
+     * 
+     * @param file - file name image
+     * @param data - string to encode into BarCode
+     * @param barcode - type of BarCode QRcode, DataMatrix
+     * @param width - width of image
+     * @param height - height of image
+     * @param hints - settings of encode BarCode to image
+     * @throws IOException
+     * @throws WriterException
+     */
     public static void writeCode(File file, String data, BarcodeFormat barcode, int width, int height, Map<EncodeHintType, Object> hints)
         throws IOException, WriterException {
         BitMatrix matrix = new MultiFormatWriter().encode(data, barcode, width, height, hints);
         MatrixToImageWriter.writeToFile(matrix, "PNG", file);
     }
 
+    /**
+     * Method to read BarCode from image
+     * 
+     * @param file - image file name
+     * @param hints - settings of read image
+     * @return - Decoded string
+     * @throws IOException
+     * @throws NotFoundException
+     */
     public static String readCode(File file, Map<DecodeHintType, ?> hints) throws IOException, NotFoundException {
         BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(file))));
         Result result = new MultiFormatReader().decode(binaryBitmap, hints);
         return result.getText();
     }
 
+    /**
+     * Create temporary file in temp directory
+     * 
+     * @param fileName
+     * @return - file
+     * @throws IOException
+     */
     public static File createFile(String fileName) throws IOException {
         File file = File.createTempFile(fileName, ".png");
         return file;
     }
 
+    /**
+     * Open file
+     * 
+     * @param file - file name
+     * @throws IOException
+     */
     private static void showFile(File file) throws IOException {
         System.out.println("printing to " + file.getAbsolutePath());
         Desktop.getDesktop().open(file);
--- src/test/java/demoTest/SimpleReadWriteBarCodeTest.java
+++ src/test/java/demoTest/SimpleReadWriteBarCodeTest.java
@@ -22,7 +22,7 @@ public class SimpleReadWriteBarCodeTest {
 
         Map<DecodeHintType, Object> hintsRead = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
         hintsRead.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.DATA_MATRIX));
-        //hintsRead.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+        hintsRead.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
         hintsRead.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
         for (int i = 1; i < 10; i++) {
             try {
@@ -41,9 +41,9 @@ public class SimpleReadWriteBarCodeTest {
 
         Map<DecodeHintType, Object> hintsRead = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
         hintsRead.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.DATA_MATRIX));
-        //hintsRead.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+        hintsRead.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
         hintsRead.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
-        for (int i = 21; i < 24; i++) {
+        for (int i = 21; i < 29; i++) {
             try {
                 File file = new File("img/" + i + ".png");
                 System.out.println(file.getAbsolutePath());
@@ -59,6 +59,7 @@ public class SimpleReadWriteBarCodeTest {
     public void getTextQr() {
         Map<DecodeHintType, Object> decodeHints = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
         decodeHints.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(BarcodeFormat.QR_CODE));
+        decodeHints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
         for (int i = 11; i < 20; i++) {
             try {
             File file = new File("img/" + i + ".png");
