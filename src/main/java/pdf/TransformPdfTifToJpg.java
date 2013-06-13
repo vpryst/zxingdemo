06363beedfea43c6171fd961db7b61c5ea85ad43
vpryst <vpryst@IF030.softservecom.com>
2013-06-13 16:13:14 +0300

    Finish write class

--- src/main/java/pdf/TransformPdfTifToJpg.java
+++ src/main/java/pdf/TransformPdfTifToJpg.java
@@ -1,33 +1,43 @@
 package pdf;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
-import com.itextpdf.text.pdf.qrcode.ByteArray;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PRStream;
 import com.lowagie.text.pdf.PdfName;
 import com.lowagie.text.pdf.PdfObject;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfStream;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.RandomAccessFileOrArray;
+import com.lowagie.text.pdf.codec.TiffImage;
 
 public class TransformPdfTifToJpg {
     private PdfReader reader;
+    private String sourceFolder;
+    private String destinationFolder;
+    private String fileName;
+    private String subFileName;
 
-    public TransformPdfTifToJpg(String filePdf) {
+    public TransformPdfTifToJpg(String sourceFolder, String destinationFolder, String fileName) {
+        this.sourceFolder = sourceFolder;
+        this.destinationFolder = destinationFolder;
+        this.fileName = fileName;
+        this.subFileName = generateSubFileName(fileName);
+        makeFolder(sourceFolder);
+        makeFolder(destinationFolder);
+    }
+
+    public void convertPdfToJpg() {
         try {
-            reader = new PdfReader(filePdf);
+            reader = new PdfReader(sourceFolder + fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public BufferedImage[] convertPdfToJpg(String path, String fileJpg) {
-        BufferedImage[] images = null;
         for (int i = 0; i < reader.getXrefSize(); i++) {
             PdfObject pdfobj = reader.getPdfObject(i);
             if (pdfobj != null) {
@@ -41,12 +51,10 @@ public class TransformPdfTifToJpg {
                             byte[] img;
                             try {
                                 img = PdfReader.getStreamBytesRaw((PRStream) stream);
-                                images[i] = ImageIO.read(new ByteArrayInputStream(img));
-                                FileOutputStream out = new FileOutputStream(new File(path + fileJpg + i + ".jpg"));
+                                FileOutputStream out = new FileOutputStream(new File(destinationFolder + subFileName + "_" + i + ".jpg"));
                                 out.write(img);
                                 out.flush();
                                 out.close();
-                                System.out.println(images[i]);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -55,10 +63,9 @@ public class TransformPdfTifToJpg {
                 }
             }
         }
-        return images;
     }
 
-    public int findPdfPageCount(String path, String fileJpg) {
+    public int findPdfPageCount() {
         int count = 0;
         for (int i = 0; i < reader.getXrefSize(); i++) {
             PdfObject pdfobj = reader.getPdfObject(i);
@@ -79,12 +86,65 @@ public class TransformPdfTifToJpg {
         return count;
     }
 
+    public void tiffToPdf() {
+        Document document = new Document();
+        RandomAccessFileOrArray ra = null;
+        try {
+
+            PdfWriter.getInstance(document, new FileOutputStream(destinationFolder + subFileName + ".pdf"));
+            document.open();
+            ra = new RandomAccessFileOrArray(sourceFolder + fileName);// "img/scaned_files/TIF_Scan/SKMBT_22313061009400.tif");
+            int pages = TiffImage.getNumberOfPages(ra);
+            for (int i = 1; i <= pages; i++) {
+                document.add(TiffImage.getTiffImage(ra, i));
+            }
+            document.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void convertTifToJpg() {
+        tiffToPdf();
+        TransformPdfTifToJpg transform = new TransformPdfTifToJpg(destinationFolder, destinationFolder + "img_jpg/", subFileName + ".pdf");
+        transform.convertPdfToJpg();
+    }
+
+    private String generateSubFileName(String fileName) {
+        int position = fileName.indexOf('.');
+        return fileName.substring(0, position);
+    }
+    private void makeFolder(String folder) {
+        File dir = new File(folder);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+    }
     /**
      * @param args
      */
     public static void main(String[] args) {
-        TransformPdfTifToJpg tr = new TransformPdfTifToJpg("img/scaned_files/pdf_scan/SimpleImages.pdf");
-        // tr.convertPdfToJpg("img/scaned_files/pdf_scan/img_temp/", "SimpleImages.pdf");
+        // TransformPdfTifToJpg tr =
+        // new TransformPdfTifToJpg("img/scaned_files/pdf_scan/", "img/scaned_files/pdf_scan/img_jpg/", "SimpleImages.pdf");
+        // tr.convertPdfToJpg();
+        File dirTif = new File("img/scaned_files/tif_scan/"); // tif_pdf
+        System.out.println(dirTif.getPath());
+        for (int i = 0; i < dirTif.listFiles().length; i++) {
+            TransformPdfTifToJpg transform =
+                new TransformPdfTifToJpg("img/scaned_files/tif_scan/", "img/scaned_files/tif_scan_pdf/", dirTif.listFiles()[i].getName());
+            transform.convertTifToJpg();
+        }
+        File dirPdf = new File("img/scaned_files/pdf_scan/"); // tif_pdf
+        System.out.println(dirPdf.getPath());
+        for (int i = 0; i < dirPdf.listFiles().length; i++) {
+            TransformPdfTifToJpg transform =
+                new TransformPdfTifToJpg("img/scaned_files/pdf_scan/", "img/scaned_files/pdf_scan_pdf/", dirPdf.listFiles()[i].getName());
+            transform.convertPdfToJpg();
+        }
     }
 
 }
